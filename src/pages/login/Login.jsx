cc978629a85f23094b28d1821066e70e49983831
chiadikaobixo <chiadikaobixo@gmail.com>
2022-05-16 15:33:09 -0500

    context and Api calls

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,6 +1,21 @@
+import React, { useContext } from 'react'
+import { useRef } from 'react'
+import { loginCall } from '../../apiCalls'
+import { AuthContext } from '../../context/authContext'
+import { CircularProgress } from '@mui/material'
 import './login.css'
 
 const Login = () => {
+    const email = useRef()
+    const password = useRef()
+    const { user, isFetching, error, dispatch } = useContext(AuthContext)
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        loginCall({ email: email.current.value, password: password.current.value }, dispatch)
+        
+    }
+
     return (
         <div className='login'>
             <div className='loginWrapper'>
@@ -11,13 +26,28 @@ const Login = () => {
                     </span>
                 </div>
                 <div className='loginRight'>
-                    <div className='loginBox'>
-                        <input placeholder='Email' className='loginInput' />
-                        <input placeholder='Password' className='loginInput' />
-                        <button className='loginButton'>LogIn</button>
-                        <span className='loginForgot'>Forget Password?</span>
+                    <form className='loginBox' onSubmit={handleClick}>
+                        <input
+                            placeholder='Email'
+                            type='email'
+                            className='loginInput'
+                            ref={email}
+                            required
+                        />
+                        <input
+                            placeholder='Password'
+                            className='loginInput'
+                            type='password'
+                            minLength='6'
+                            ref={password}
+                            required
+                        />
+                        <button className='loginButton' type='submit' disabled={isFetching}>
+                            {isFetching ? <CircularProgress size="20px" /> : "LogIn"}
+                        </button>
+                        <span className='loginForgot'>Forgot Password?</span>
                         <button className='loginRegisterButton'>Create a New Account</button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
